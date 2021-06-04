8257859dfaea54c8701e1d22f9f20a0522f0c5d9
Scott Hutley <scotthutley1@comcast.net>
2021-06-04 11:55:57 -0700

    create let/if within document js to make 0 quote increase parameters

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     const age = parseInt($("input#age").val());
     const gender = $("select#gender").val();
 
-    const quote = (100 - age) * 3;
+    let quote = (100 - age) * 3;
+    if (gender=== 'male' || age < 26) {
+      quote += 50;
+    }
 
     $("#rate").text(quote);
     $("#quote").show();
